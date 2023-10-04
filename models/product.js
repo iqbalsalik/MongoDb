66af864a5737efffa4fc336e169c669d73b4f134
iqbalsalik <sisalik84@gmail.com>
2023-10-04 15:00:49 +0530

    Delete and Update functionality

--- models/product.js
+++ models/product.js
@@ -1,25 +1,69 @@
+const mongodb = require("mongodb")
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(id,title,price,imageUrl,description){
-    this.id = id,
-    this.title = title,
-    this.price = price,
-    this.imageUrl = imageUrl,
-    this.description = description
+  constructor(title,imageUrl,price, description,id) { 
+      this.title = title,
+      this.imageUrl = imageUrl,
+      this.description = description,
+      this.price = price,
+      this._id = id ? new mongodb.ObjectId(id) : null
   }
 
-  save(){
+  save() {
     const db = getDb();
-    return db.collection("products")
-    .insertOne(this)
-    .then(result=>{
-      console.log("yessssssssssss")
-      console.log(result)
+    let dbOp;
+    if(this._id){
+      dbOp = db.collection("products").updateOne({_id: this._id},{$set:this})
+    }else{
+      dbOp = db.collection("products")
+      .insertOne(this)
+    }
+    return dbOp
+      .then(result => {
+        console.log(result)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  static fetchAll(){
+    const db = getDb();
+    return db.collection("products").find().toArray()
+    .then(products=>{
+      console.log(products);
+      return products
     })
     .catch(err=>{
       console.log(err)
     })
+  }
+
+  static findById(prodId){
+    const db = getDb();
+    return db.collection("products").find({_id: new mongodb.ObjectId(prodId)}).next()
+    .then(product=>{
+      console.log(product);
+      return product
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+
+  static deleteById(prodId){
+    const db = getDb();
+    if(prodId){
+      return db.collection("products").deleteOne({_id: new mongodb.ObjectId(prodId)})
+      .then(product=>{
+        console.log("Deleted")
+      }).catch(err=>{
+        console.log(err)
+      })
+    }else{
+      return "Nothing to Delete!"
+    }
   }
 }
 
