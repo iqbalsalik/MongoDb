7ab9e6d4bf89a5a04b4ebd44d0daae4324572ef1
iqbalsalik <sisalik84@gmail.com>
2023-10-05 16:03:07 +0530

    Add to Cart

--- models/product.js
+++ models/product.js
@@ -21,19 +21,12 @@ class Product {
       .insertOne(this)
     }
     return dbOp
-      .then(result => {
-        console.log(result)
-      })
-      .catch(err => {
-        console.log(err)
-      })
   }
 
   static fetchAll(){
     const db = getDb();
     return db.collection("products").find().toArray()
     .then(products=>{
-      console.log(products);
       return products
     })
     .catch(err=>{
--- models/user.js
+++ models/user.js
@@ -2,14 +2,33 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(userName,email){
+  constructor(userName,email,cart,id){
     this.name = userName,
-    this.email = email
+    this.email = email,
+    this.cart = cart,
+    this._id = id
   }
 
   save(){
     const db  = getDb();
     return db.collection("users").insertOne(this)
+  }
+
+  addToCart(product){
+    let newQuantity=1
+    const cartProductIndex = this.cart.items.findIndex(cp=>{
+      return cp.productId.toString()===product._id.toString() 
+    })
+    const updatedCartItems = [...this.cart.items]
+    if(cartProductIndex>=0){
+      newQuantity = this.cart.items[cartProductIndex].quantity+1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity
+    }else{
+      updatedCartItems.push({productId:product._id, quantity:newQuantity})
+    }
+    let updatedCart = {items:updatedCartItems};
+    let db = getDb();
+    return db.collection("users").updateOne({_id: new mongodb.ObjectId(this._id)},{$set: {cart:updatedCart}})
   }
 
   static findById(userId){
